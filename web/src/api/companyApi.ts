effff9df0ab3324d764ad7f77c043e6890d29b4e
aking526 <asking52607@gmail.com>
2022-09-03 22:15:46 -0400

    Update companyApi.ts

--- web/src/api/companyApi.ts
+++ web/src/api/companyApi.ts
@@ -1,23 +1,24 @@
 import axios from "axios";
+import ISS from "../types/ISS";
 
 const apiClient = axios.create({
-	baseURL: "http://localhost:8000/companies",
-	headers: {
-		"Content-type": "application/json"
-	}
+  baseURL: "http://localhost:8000/companies",
+  headers: {
+    "Content-type": "application/json"
+  }
 });
 
 const getNames = async () => {
-	const res = await apiClient.get<{[index: string]: string}>("/getNames");
-	const data = res.data;
+  const res = await apiClient.get<ISS>("/getNames");
+  const data = res.data;
 
-	const f: string[][] = [[]];
-	f.pop();
-	for (const ticker in data) {
-		f.push([ticker, data[ticker]]);
-	}
+  const f: string[][] = [[]];
+  f.pop();
+  for (const ticker in data) {
+    f.push([ticker, data[ticker]]);
+  }
 
-	return f;
+  return f;
 };
 
 const CompanyApi = { getNames };
