ed35083d8dfc3f2539e932880809c8b17375c448
aking526 <asking52607@gmail.com>
2022-10-18 23:02:08 -0400

    Update esg data

--- server/src/scripts/cacheData.ts
+++ server/src/scripts/cacheData.ts
@@ -75,7 +75,7 @@ const ExecEsgBatch = async (dayCnt: number, customList: string[] | undefined) =>
 
 	const getNewList = (list: string[]) => {
 		let arr = [];
-		for (let i = list.length - 1; i >= 0; i--) {
+		for (let i = 0; i < list.length; i++) {
 			const ticker = list[i];
 			if (ticker in e) continue;
 			if (arr.length >= 50) break;
