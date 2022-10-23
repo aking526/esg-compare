c96b8f4cd9dc6c07d54febb86e3723f9c81ab2ad
aking526 <asking52607@gmail.com>
2022-10-22 23:25:31 -0400

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -52,7 +52,7 @@ const StartServer = () => {
 	// Incorporate Frontend
 	app.use(express.static(path.join(__dirname, '../../../web/build')));
 
-	app.get("/*", (req: Request, res: Response) => {
+	app.get("/", (req: Request, res: Response) => {
 		res.sendFile(path.join(__dirname, '../../../web/build', 'index.html'));
 	});
 
