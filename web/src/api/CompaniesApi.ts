187cad281a3e92c4ef0435d481a6bcd73b366571
aking526 <asking52607@gmail.com>
2022-10-23 23:07:04 -0400

    Update .gitignore

---
+++ web/src/api/CompaniesApi.ts
@@ -0,0 +1,74 @@
+import axios from "axios";
+import ISS from "../types/ISS";
+import { ICompanyData } from "../types/ICompanyData";
+import { IScores, IGrades, ILevels } from "../types/ESGDataInterfaces";
+
+// const apiClient = axios.create({
+//   baseURL: "/api/companies",
+//   headers: {
+//     "Content-type": "application/json"
+//   }
+// });
+
+// Production
+const startUrl = "/api/companies";
+
+// Development
+// const startUrl = "http://localhost:8000/api/companies";
+
+const getNames = async () => {
+  const ak = process.env.REACT_APP_AK;
+  const res = await axios.get<ISS>(startUrl + `/getNames&token=${ak}`);
+  const data = res.data;
+
+  const f: string[][] = [[]];
+  f.pop();
+  for (const ticker in data) {
+    f.push([ticker, data[ticker]]);
+  }
+
+  return f;
+};
+
+const getIndustries = async () => {
+  const ak = process.env.REACT_APP_AK;
+  const res = await axios.get<string[]>(startUrl + `/getIndustries&token=${ak}`);
+  return res.data;
+};
+
+const fetchRankings = async (metric: string, filters: string | null) => {
+  const ak = process.env.REACT_APP_AK;
+  const url = startUrl + (filters ? `/sort/${metric}&token=${ak}?${filters}` : `/sort/${metric}&token=${ak}`);
+  const res = await axios.get<ICompanyData[]>(url);
+  return res.data
+};
+
+const fetchCompanyData = async (ticker: string | undefined) => {
+  const ak = process.env.REACT_APP_AK;
+  const res = await axios.get<ICompanyData>(startUrl + `/get/${ticker}&token=${ak}`);
+  return res.data;
+};
+
+const getScores = async (industry: string | string[] | null | undefined ) => {
+  const ak = process.env.REACT_APP_AK;
+  const url = startUrl + (industry ? `/getScores&token=${ak}?industry=${typeof industry === "string" ? industry : industry.join(",")}` : `/getScores&token=${ak}`);
+  const res = await axios.get<IScores[]>(url);
+  return res.data;
+};
+
+const getGrades = async (industry: string | string[] | null | undefined) => {
+  const ak = process.env.REACT_APP_AK;
+  const url = startUrl + (industry ? `/getGrades&token=${ak}?industry=${typeof industry === "string" ? industry : industry.join(",")}` : `/getGrades&token=${ak}`);
+  const res = await axios.get<IGrades[]>(url);
+  return res.data;
+};
+
+const getLevels = async (industry: string | string[] | null | undefined ) => {
+  const ak = process.env.REACT_APP_AK;
+  const url = startUrl + (industry ? `/getLevels&token=${ak}?industry=${typeof industry === "string" ? industry : industry.join(",")}` : `/getLevels&token=${ak}`);
+  const res = await axios.get<ILevels[]>(url);
+  return res.data;
+};
+
+const CompaniesApi = { getNames, getIndustries, fetchCompanyData, fetchRankings, getScores, getGrades, getLevels };
+export default CompaniesApi;
---
+++ web/src/api/StockApi.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+import ISA from "../types/ISA";
+import { IBasicFinancials, IStockQuote } from "../types/StockFinancialInterfaces";
+
+// const apiClient = axios.create({
+// 	baseURL: "/api/stockInfo",
+// 	headers: {
+// 		"Content-type": "application/json"
+// 	}
+// });
+
+// Production
+const startUrl = "/api/stockInfo"
+
+// Development
+// const startUrl = "http://localhost:8000/api/stockInfo";
+
+const fetchStockInfo = async (ticker: string | undefined, resolution: string, from: string | number, to: string | number) => {
+	const ak = process.env.REACT_APP_AK;
+	const res = await axios.get<ISA>(startUrl + `/get/symbol=${ticker}&resolution=${resolution}&from=${from}&to=${to}&token=${ak}`);
+	return res.data;
+};
+
+const getQuote = async (ticker: string) => {
+	const ak = process.env.REACT_APP_AK;
+	const res = await axios.get<IStockQuote>(startUrl + `/getQuote/${ticker}&token=${ak}`);
+	return res.data;
+};
+
+const getBasicFinancials = async (ticker: string) => {
+	const ak = process.env.REACT_APP_AK;
+	const res = await axios.get<IBasicFinancials>(startUrl + `/getBasicFinancials/${ticker}&token=${ak}`);
+	return res.data;
+};
+
+const getNews = async (ticker: string | undefined, from: string, to: string) => {
+	const ak = process.env.REACT_APP_AK;
+	const res = await axios.get(startUrl + `/getNews/${ticker}/from=${from}&to=${to}&token=${ak}`);
+	return res.data;
+};
+
+const StockApi = { fetchStockInfo, getQuote, getBasicFinancials, getNews };
+export default StockApi;
