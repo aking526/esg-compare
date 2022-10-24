b3101b3db789f9165636a081541382ae687c5d0a
aking526 <asking52607@gmail.com>
2022-10-23 23:10:06 -0400

    Update Company.routes.ts

--- server/src/routes/Company.routes.ts
+++ server/src/routes/Company.routes.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 /* CRUD Routes */
 router.post("/create/auth=:auth", ValidateSchema(schemas.company.create), companyController.createCompany);
 router.get("/get/:ticker&token=:key", companyController.readCompany);
-router.get("/get&token:key", companyController.readAll);
+router.get("/get&token=:key", companyController.readAll);
 router.patch("/update/ticker=:ticker&auth=:auth", ValidateSchema(schemas.company.update), companyController.updateCompany);
 router.delete("/delete/ticker=:ticker&auth=:auth", companyController.deleteCompany);
 router.delete("/deleteById/id=:id&auth=:auth", companyController.deleteById);
