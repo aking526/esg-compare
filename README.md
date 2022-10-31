38eb377f309f37f53768b852e314d349ec99be78
aking526 <asking52607@gmail.com>
2022-10-31 00:47:53 -0400

    About page updates

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # ESG Compare
 
 ## Our Purpose 
-This website was created to provide the ESG ratings information of some of the largest publicly traded companies.  It reflects the ESG ratings of 620 public companies listed on either the NYSE or Nasdaq, most of which are included in the S&P 500.
+This website was created to provide the ESG ratings information of some of the largest publicly traded companies in the United States. It reflects the ESG ratings of hundreds of public companies listed on either the NYSE or Nasdaq, most of which are included in the S&P 500.
 
 The mission of this website is to: 
 1. Raise awareness of the importance of sustainability
@@ -10,9 +10,9 @@ The mission of this website is to:
 
 
 ## Tips for Users
-* For a company’s own description of its ESG practices, click on the company SEC filings and select the company’s most recent proxy statement for its annual meeting where you will find disclosures about the company’s governance matters.
-* Additionally, click on the company’s own website and navigate to its investor relations page where many big public companies publish their own sustainability reports.
-* If you want to contact a company’s board of directors with concerns about the company’s ESG performance, you can find the contact information for the company’s board of directors on its investor relations pages (often under the Board Leadership page) or in its proxy statement.
+* For a company’s own description of its ESG practices, click on the company SEC filings link on the company profile page and select the company’s most recent proxy statement for its annual meeting where you will find disclosures about the company’s governance matters.
+* Additionally, click on the link to the company’s own website on the company profile page and navigate to its investor relations page. On the investor relations pages, you will find lots of additional information about the company, including, in some cases, their own sustainability reports.
+* To contact a company’s board of directors with concerns about the company’s ESG performance, search on the company’s investor relations page or in the company’s proxy statement for the contact information for the company’s board of directors. Many companies separately include this information to allow interested parties to contact their board.
 
 
 ## License
--- web/src/pages/About.tsx
+++ web/src/pages/About.tsx
@@ -9,16 +9,16 @@ const About: React.FC = () => {
         <div className="flex flex-col bg-contrast-gray mt-8 hover:shadow-light p-5">
           <h3 className="text-4xl">Our Purpose</h3>
           <div className="flex flex-col mt-2 text-xl">
-            <p className="mb-1">This website was created to provide the ESG ratings information of some of the largest publicly traded companies.  It reflects the ESG ratings of 620 public companies listed on either the NYSE or Nasdaq, most of which are included in the S&P 500.</p>
-            <p>The mission of this website is to: raise awareness of the importance of sustainability, facilitate socially responsible investing, and incentivize companies to create value for our society through responsible and ethical business practices.</p>
+            <p className="mb-1.5">This website was created to provide the ESG ratings information of some of the largest publicly traded companies in the United States. It reflects the ESG ratings of hundreds of public companies listed on either the NYSE or Nasdaq, most of which are included in the S&P 500.</p>
+            <p className="mt-1.5">The mission of this website is to: raise awareness of the importance of sustainability, facilitate socially responsible investing, and incentivize companies to create value for our society through responsible and ethical business practices.</p>
           </div>
         </div>
         <div className="flex flex-col bg-contrast-gray mt-8 hover:shadow-light p-5">
           <h3 className="text-4xl">Tips for Users</h3>
           <ul className="mt-2 text-xl">
-            <li className="mb-1">&#x2022; For a company’s own description of its ESG practices, click on the company SEC filings and select the company’s most recent proxy statement for its annual meeting where you will find disclosures about the company’s governance matters.</li>
-            <li className="mb-1">&#x2022; Additionally, click on the company’s own website and navigate to its investor relations page where many big public companies publish their own sustainability reports.</li>
-            <li className="mb-1">&#x2022; If you want to contact a company’s board of directors with concerns about the company’s ESG performance, you can find the contact information for the company’s board of directors on its investor relations pages (often under the Board Leadership page) or in its proxy statement.</li>
+            <li className="mb-1.5">&#x2022; For a company’s own description of its ESG practices, click on the company SEC filings link on the company profile page and select the company’s most recent proxy statement for its annual meeting where you will find disclosures about the company’s governance matters.</li>
+            <li className="mb-1.5">&#x2022; Additionally, click on the link to the company’s own website on the company profile page and navigate to its investor relations page. On the investor relations pages, you will find lots of additional information about the company, including, in some cases, their own sustainability reports.</li>
+            <li className="mb-1.5">&#x2022; To contact a company’s board of directors with concerns about the company’s ESG performance, search on the company’s investor relations page or in the company’s proxy statement for the contact information for the company’s board of directors. Many companies separately include this information to allow interested parties to contact their board.</li>
           </ul>
         </div>
         <div className="flex flex-col mt-10 bg-contrast-gray hover:shadow-light p-5">
